49660554d936927c30827b41c303f04e2ab6e97d
Jason Norwood-Young <jason@10layer.com>
2019-05-21 10:23:59 +0200

    Total rewrite for new Gallo API

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AFP API
 
-This app grabs articles from News24Wire via Gallo and posts them on to an API. You will need a Gallow/News24Wire account to use it.
+This app grabs articles from News24Wire via Gallo and posts them on to an API. You will need a Gallo/News24Wire account to use it.
 
 It doesn't stay resident, so I suggest putting it into a cron. It can take a while to go through all the articles, so I suggest a cron time of around 5 mins to be safe.
 
@@ -13,8 +13,8 @@ It doesn't stay resident, so I suggest putting it into a cron. It can take a whi
 Create a `.env` file with the following in it:
 
 ```
-USERNAME=<your AFP username>
-PASSWORD=<your AFP password>
+USERNAME=<your Gallo username>
+PASSWORD=<your Gallo password>
 URL=http://www.galloimages.co.za/NewsWire?sitegroupID=20
 LOGIN_URL=http://www.galloimages.co.za/Account/LogOn
 API_USERNAME=<your RESTful API username>
@@ -25,15 +25,3 @@ API_ENDPOINT=<url to the API's endpoint to store the articles>
 ## Running
 
 `npm start`
-
-## Troubleshooting
-
-If you get an error like `Running as root without --no-sandbox is not supported.`, create another user to run this app and make sure this user has permission to run Chrome here.
-
-```
-adduser my-user
-chmod -R u+x node_modules/puppeteer/.local-chromium/linux-515411/chrome-linux/
-chown my-user -R node_modules/puppeteer/.local-chromium/linux-515411/chrome-linux/
-```
-
-Then `su my-user` and set up your Cron in there. 
--- index.js
+++ index.js
@@ -1,73 +1,49 @@
-const puppeteer = require("puppeteer");
 require("dotenv").config();
-const rest = require("restler-bluebird");
-const moment = require("moment");
+const axios = require("axios");
 
 (async () => {
-	var storedArticleData = await rest.get(process.env.API_ENDPOINT + `?fields=provider_uid&filter[provider]=News24`, {
-		username: process.env.API_USERNAME,
-		password: process.env.API_PASSWORD
-	});
-	var storedArticles = storedArticleData.data;
-	
-	// Load the site
-	const browser = await puppeteer.launch({ headless: true, timeout: 5000 });
-	const page = await browser.newPage();
-	
-	console.log("Loading url", process.env.LOGIN_URL);
-	await page.goto(process.env.LOGIN_URL);
-	
-	// Login
-	await page.waitForSelector("#username");
-
-	await page.type("#username", process.env.USERNAME);
-	await page.type("#password", process.env.PASSWORD);
-	await page.click("#login-form > div.row-fluid > div:nth-child(1) > div:nth-child(6) > div > input");
-	await page.waitForSelector("#header-sign-in");
-	
-	console.log("Loading url", process.env.URL);
-	await page.goto(process.env.URL);
-	await page.waitForSelector(".newswire-header");
-
-	let articleElements = await page.$$(".newswire-header");
-	let articles = [];
-	let cookies = await page.cookies();
-	let aspSessId = null;
-	for (let cookie of cookies) {
-		let name = await cookie.name;
-		if (name === "ASP.NET_SessionId")
-			aspSessId = await cookie.value;
-	}
-	for (let articleEl of articleElements) {
-		let urlEl = await articleEl.getProperty("href");
-		let url = await urlEl.jsonValue();
-		let id = url.replace(process.env.URL + "#", "");
-		let exists = storedArticles.find(
-			storedArticle =>
-				storedArticle.provider_uid === id
-		);
-		if (!exists) {
-			console.log({ id });
-			let article = await rest.get(`http://www.galloimages.co.za/newswire/download?id=${ id }&extension=json`, { headers: { Host: "www.galloimages.co.za", Cookie: `ASP.NET_SessionId=${aspSessId}` }});
-			let jsonResponse = JSON.parse(article);
-			let data = {
-				headline: jsonResponse.headline,
-				blurb: jsonResponse.slugline,
-				provider_uid: id,
-				body: jsonResponse.body.value,
-				byline: jsonResponse.author,
-				provider: "News24",
-				date: jsonResponse.dateCreated,
-				keywords: jsonResponse.tags,
-			}
-			await rest.post(process.env.API_ENDPOINT, {
-				data,
+	try {
+		const url = `${process.env.API_ENDPOINT}?fields=provider_uid&filter[provider]=News24&limit=100&sort[date_created]=-1`;
+		var already_fetched = (await axios.get( url, {
+			auth: {
 				username: process.env.API_USERNAME,
 				password: process.env.API_PASSWORD
-			});
+			}
+		})).data.data;
+		
+		const login = (await axios.post(`${process.env.URL}/accounts/login`, {
+			userName: process.env.USERNAME,
+			password: process.env.PASSWORD
+		})).data;
+		console.log(login);
+		const headers = {'Authorization': `bearer ${login.token}`}
+		const article_list = (await axios.get(`${process.env.URL}/newsfeeds/stories/breakingauth?q=&fromDate=0&toDate=${new Date().getTime() }&limit=100`, { headers })).data.filter(article => (already_fetched.find(fetched => fetched.provider_uid === article.id) == undefined));
+		console.log(article_list);
+		for (let article_meta of article_list) {
+			let article = (await axios.get(`${process.env.URL}/newsFeeds/${article_meta.id}`, { headers })).data;
+			let data = {
+				headline: article.headline,
+				blurb: article.slugline,
+				provider_uid: article.id,
+				body: article.body,
+				byline: article.author,
+				provider: "News24",
+				date: article.dateCreated,
+				keywords: article.keywords,
+			}
+			console.log(data);
+			await axios.post(process.env.API_ENDPOINT,
+				data,
+				{ 
+					auth: {
+						username: process.env.API_USERNAME,
+						password: process.env.API_PASSWORD
+					}
+				}
+			);
 		}
+		console.log("Done")
+	} catch(err) {
+		console.error(err);
 	}
-
-	console.log("Done")
-	browser.close();
 })();
